4200b43fa58a8c52941fe046a4a2b68cc74420f1
Derek Yadlowski <Derek.Yadlowski@sait.ca>
2024-09-13 10:32:55 -0600

    Week 2 Content

---
+++ app/week-2/page.js
@@ -0,0 +1,11 @@
+import MyFirstComponent from "../_components/my-first-component";
+
+export default function Page() {
+    return (
+        <main>
+            <h1>My Second Page</h1>
+            <p>This is the second page in my React app.</p>
+            <MyFirstComponent />
+        </main>
+    );
+}
