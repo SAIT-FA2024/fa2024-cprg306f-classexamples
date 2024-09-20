b5cbb949adb99b1874bc905044acdf2800c2d4dd
Derek Yadlowski <Derek.Yadlowski@sait.ca>
2024-09-20 09:50:15 -0600

    Week 3 Content

--- app/page.js
+++ app/page.js
@@ -6,6 +6,8 @@ import MyFirstComponent from "./_components/my-first-component";
 
 export default function Home() {
 
+  let linkStyles = "underline text-cyan-600 hover:text-cyan-300";
+
   return (
     <main>
       <h1 className="text-3xl">CPRG306 Class Examples</h1>
@@ -13,12 +15,9 @@ export default function Home() {
       <MyFirstComponent />
       <h2>Course Example Links</h2>
       <ul>
-        <li>
-          <Link href="./week-2/" className="underline text-cyan-600 hover:text-cyan-300">Week 2 - Introduction to React</Link>
-        </li>
-        <li>
-          <Link href="./week-3/" className="underline text-cyan-600 hover:text-cyan-300">Week 3</Link>
-        </li>
+        <li><Link href="./week-2/" className={linkStyles}>Week 2 - Introduction to React</Link></li>
+        <li><Link href="./week-3/objects/" className={linkStyles}>Week 3 - Objects</Link></li>
+        <li><Link href="./week-3/props/" className={linkStyles}>Week 3 - Props</Link></li>
       </ul>
     </main>
   );
