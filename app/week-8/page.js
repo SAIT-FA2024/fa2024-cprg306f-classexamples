e8d27e83fcc9ce2c85634c20f0b6277e42982445
Derek Yadlowski <Derek.Yadlowski@sait.ca>
2024-10-25 10:08:20 -0600

    Week 8 Content

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -1,10 +1,12 @@
+import Gallery from "./gallery";
 import SingleArt from "./single-art";
 
 export default function PageGallery(){
 
     return(
         <main>
-            <SingleArt />
+            {/* <SingleArt /> */}
+            <Gallery />
         </main>
     );
 }
